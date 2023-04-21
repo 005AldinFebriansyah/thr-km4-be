57d169570c8d81730d3a08f89a0ea59064b5b408
Aldin Febriansyah <aldin.20005@mhs.unesa.ac.id>
2023-04-21 13:30:58 +0700

    done quest: add-new-element

--- add-new-element/main.go
+++ add-new-element/main.go
@@ -1,9 +1,24 @@
 package main
 
-func AddElement(data []int, newData int, position string) []int {
-	//your code here!
+import "fmt"
 
-	return nil
+func AddElement(data []int, newData int, position string) []int {
+	if position == "up" {
+		newDataSlice := []int{newData}
+		data = append(newDataSlice, data...)
+	} else if position == "down" {
+		data = append(data, newData)
+	} else {
+		return nil
+	}
+	return data
 }
 
-func main() {}
+func main() {
+	arr := []int{1, 2, 3, 4, 5}
+	newArr := AddElement(arr, 6, "up")
+	fmt.Println(newArr) // [6 1 2 3 4 5]
+
+	newArr = AddElement(arr, 6, "down")
+	fmt.Println(newArr) // [1 2 3 4 5 6]
+}
