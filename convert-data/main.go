fcc1e54a2e058341750d1bb10a7ca99db3b3f378
Aldin Febriansyah <aldin.20005@mhs.unesa.ac.id>
2023-04-21 13:36:45 +0700

    done quest: convert-data

--- convert-data/main.go
+++ convert-data/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type User struct {
 	Name    string
 	Age     int
@@ -7,9 +13,17 @@ type User struct {
 }
 
 func ConvertData(data string) User {
-	//your code here!
-
-	return User{}
+	fields := strings.Split(data, ",")
+	age, _ := strconv.Atoi(fields[1])
+	return User{
+		Name:    fields[0],
+		Age:     age,
+		Address: fields[2],
+	}
 }
 
-func main() {}
+func main() {
+	data := "Edo,20,Jakarta"
+	user := ConvertData(data)
+	fmt.Println(user)
+}
