64bb7686a765065f0b8b130cfb7fa395824342dc
Aldin Febriansyah <aldin.20005@mhs.unesa.ac.id>
2023-04-21 13:40:37 +0700

    done quest: count-data-slice

--- count-data-slice/main.go
+++ count-data-slice/main.go
@@ -1,9 +1,21 @@
 package main
 
-func howManyElements(data []any) int {
-	//your code here!
-
-	return 0
+func howManyElements(data []interface{}) int {
+	return len(data)
 }
 
-func main() {}
+func main() {
+	data1 := []interface{}{1, 2, 3, 4, 5}
+	data2 := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	data3 := []interface{}{1, 1, 1, 5, 5, 5}
+	data4 := []interface{}{"Edo", "Budi", "Joko", "Tono"}
+	data5 := []interface{}{"Edo", "Budi", "Joko", "Tono", "Edo", "Budi", "Joko", "Tono"}
+	data6 := []interface{}{true, false, true, false, true, false}
+
+	println(howManyElements(data1)) // Output: 5
+	println(howManyElements(data2)) // Output: 10
+	println(howManyElements(data3)) // Output: 6
+	println(howManyElements(data4)) // Output: 4
+	println(howManyElements(data5)) // Output: 8
+	println(howManyElements(data6)) // Output: 6
+}
