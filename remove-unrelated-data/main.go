d22e91090c9408a0cc3598a1b306741324fd02c9
Aldin Febriansyah <aldin.20005@mhs.unesa.ac.id>
2023-04-21 13:55:18 +0700

    done quest: remove-unrelated-data

--- remove-unrelated-data/main.go
+++ remove-unrelated-data/main.go
@@ -1,9 +1,23 @@
 package main
 
-func removeUnrelated(dataMap map[string]any, key string) map[string]any {
-	//your code here
+import "fmt"
 
-	return nil
+func removeUnrelated(dataMap map[string]any, key string) map[string]any {
+	delete(dataMap, key)
+	return dataMap
 }
 
-func main() {}
+func main() {
+	data := map[string]any{
+		"satu":  "ichi",
+		"dua":   "ni",
+		"tiga":  "san",
+		"empat": "yon",
+		"lima":  "go",
+	}
+	fmt.Println("Data awal:", data)
+
+	keyToRemove := "tiga"
+	result := removeUnrelated(data, keyToRemove)
+	fmt.Printf("Data setelah key '%s' dihapus: %v\n", keyToRemove, result)
+}
